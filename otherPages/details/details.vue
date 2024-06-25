8346f2713e1d73621f846aaa546bee56678bbcc6
jiaqi <2468260248@qq.com>
2024-06-25 11:21:25 +0800

    中医历史与文化、中医健康专家 列表/详情页面

---
+++ otherPages/details/details.vue
@@ -0,0 +1,316 @@
+<template>
+  <view class="details-view">
+    <view class="details-tit">
+      {{detailsForm.node_name||detailsForm.expert_name||''}}
+    </view>
+    <view class="details-msg">
+      <view class="author" v-if="detailsForm.author">
+        作者：<text>{{detailsForm.author || '-'}}</text>
+      </view>
+  <!--    <view class="author" v-if="detailsForm.expert_name">
+        姓名：<text>{{detailsForm.expert_name || '-'}}</text>
+      </view> -->
+      <view class="publish" v-if="detailsForm.author">
+        发布：<text>{{detailsForm.create_time}}</text>
+      </view>
+      <view class="publish" v-if="detailsForm.expert_title">
+        职称/标签：<text>{{detailsForm.expert_title}}</text>
+      </view>
+    </view>
+    <view class="padding-tb" v-if="detailsForm.expert_expertise">
+       <textarea style="width: 100%;" :value="detailsForm.expert_expertise" disabled placeholder="" />
+    </view>
+    <view class="richText" v-if="detailsForm.node_desc">
+      <!-- <rich-text :nodes="detailsForm.contents"></rich-text> -->
+      <u-parse :html="detailsForm.node_desc" :selectable="true" :tag-style="style"></u-parse>
+    </view>
+    <view class="richText" v-else>
+      <!-- <rich-text :nodes="detailsForm.contents"></rich-text> -->
+      <image :src="detailsForm.coverFileName" mode=""></image>
+    </view>
+    <!-- 		<view class="look-over">
+			<uni-icons type="eye" size="30"></uni-icons>
+			<view class="look-tit" style="margin-right: 200rpx;">浏览量（333）</view>
+			<uni-icons type="star" size="30" :color="sc == 2 ? '#faa51b' : ''" v-if="sc == 1" @click="scang(2)"></uni-icons>
+			<uni-icons type="star" size="30" :color="sc == 2 ? '#faa51b' : ''" v-else @click="scang(1)"></uni-icons>
+			<view class="look-tit" style="margin-right: 40rpx;" v-if="sc == 1" @click="scang(2)">收藏</view>
+			<view class="look-tit" style="margin-right: 40rpx;" v-else @click="scang(1)">已收藏</view>
+			<uni-icons type="redo" size="30"></uni-icons>
+			<view class="look-tit">分享</view>
+		</view> -->
+    <!-- <view style="margin: 20rpx 0;">相关推荐
+		</view>
+		<view class="recommend">
+			<view class="recommend-item" v-for="item in list">
+				<img class="recommend-img" src="../../static/img/tuijian.jpg" alt="">
+				<view class="recommend-tit">
+					糖尿病会造成急性或慢性并发症
+				</view>
+			</view>
+		</view> -->
+    <!-- <view class="review">
+			<view class="review-top">
+				<view class="review-left">
+					评论（344）
+				</view>
+				<view class="review-icon">
+					<uni-icons type="compose" size="30"></uni-icons>
+				</view>
+				<view class="review-right">
+					参与评论
+				</view>
+			</view>
+			<view class="review-list" v-for="i in 4">
+				<img class="review-img" src="http://139.224.68.50:9017/file/18114b7680374a7b806e962aea7859b7.jpg"
+					alt="">
+				<view class="content">
+					<view class="content-top">
+						<view class="user-name">
+							微信用户
+						</view>
+						<view class="review-time">
+							2023-10-10
+						</view>
+					</view>
+					<view class="review-content">
+						金秋的阳光温馨安静，金秋的微风和煦温柔，金秋的蓝天白云飘逸，金秋的田野遍地金黄。在这个硕果累累的季节
+					</view>
+				</view>
+			</view>
+		</view> -->
+  </view>
+</template>
+
+<script>
+  import uParse from '@/components/u-parse/u-parse.vue'
+  // import * as topNewsList from '../../common/api/topNewsList.js'
+  // import * as me from '../../common/api/me.js'
+  export default {
+    components: {
+      uParse
+    },
+    data() {
+      return {
+        classify: "",
+        uid: '',
+        list: [1, 2, 3, 4],
+        detailsForm: {
+          titleName: ''
+        },
+        style: {
+          // 字符串的形式
+          img: 'display: block;',
+          video: 'display: block;width:670rpx;'
+        },
+        sc: 1
+      };
+    },
+    onLoad(e) {
+      this.classify = e.classifyName
+      // this.uid = e.uid
+      this.getData(e.id)
+    },
+    methods: {
+      // 收藏或已收藏
+      // scang(type){
+      // 	let param = {}
+      // 	if(type == 1){
+      // 		param = {
+      // 			uid:this.detailsForm.collectId
+      // 		};
+      // 	}else{
+      // 		param = {
+      // 			type:2,
+      // 			openId:uni.getStorageSync('openid_key'),
+      // 			targetId:this.uid
+      // 		};
+      // 	}
+
+      // 	me.collectInsert(param).then(res => {
+      // 		if(res.data.code == 200){
+      // 			this.sc = type
+      // 			this.getData();
+      // 		}
+      // 	})
+      // },
+      getData(id) {
+        const serviceMap = {
+          '中医健康科普专家': 'srvhealth_chinese_medicine_expert_select',
+          '中医历史与文化': 'srvhealth_knowledge_node_select'
+        }
+        const url = this.getServiceUrl('health', serviceMap[this.classify], 'select');
+        // const url = `/health/select/srvhealth_knowledge_node_select`
+        const req = {
+          "serviceName": serviceMap[this.classify],
+          "colNames": ["*"],
+          "condition": [{
+            "colName": "id",
+            "value": id,
+            "ruleType": "eq"
+          }],
+          "page": {
+            "pageNo": 1,
+            "rownumber": 1
+          }
+        }
+        this.$http.post(url, req).then(res => {
+          if (res.data.state === 'SUCCESS') {
+            if (res?.data?.data?.length) {
+              this.detailsForm = res.data.data[0]
+              if (this.detailsForm.node_name || this.detailsForm.expert_name) {
+                uni.setNavigationBarTitle({
+                  title: this.detailsForm.node_name|| this.detailsForm.expert_name
+                })
+              }
+            }
+          }
+        })
+
+        // let param = {
+        // 	uid: this.uid,
+        // 	openId:uni.getStorageSync('openid_key')
+        // }
+        // topNewsList.queryById(param).then(res => {
+        // 	this.detailsForm = res.data.data
+        // 	this.sc = res.data.data.collectId == null ? 1 : 2;
+        // }).catch(error => {
+        // 	console.log(error)
+        // })
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .details-view {
+    padding: 50rpx 20rpx;
+
+    .details-tit {
+      text-align: center;
+      font-size: 40rpx
+    }
+
+    .details-msg {
+      display: flex;
+      justify-content: space-between;
+      font-size: 24rpx;
+      margin-top: 30rpx;
+    }
+
+    .look-over {
+      display: flex;
+      font-size: 24rpx;
+      line-height: 40rpx;
+      margin: 20rpx 0;
+
+      .look-tit {
+        margin-bottom: 10rpx;
+      }
+
+      .uni-icons {
+        font-size: 24rpx;
+      }
+    }
+
+    .recommend {
+      width: 710rpx;
+      display: flex;
+      gap: 40rpx;
+      overflow-x: scroll;
+
+      .recommend-item {
+        width: 200rpx;
+
+        .recommend-img {
+          width: 200rpx;
+          height: 140rpx;
+          border-radius: 20rpx;
+        }
+
+        .recommend-tit {
+          width: 200rpx;
+          overflow: hidden;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+        }
+      }
+
+    }
+
+    .review {
+      width: 710rpx;
+      margin-top: 40rpx;
+
+      .review-top {
+        position: relative;
+        height: 80rpx;
+
+        .review-left {
+          position: absolute;
+          left: 0;
+        }
+
+        .review-icon {
+          position: absolute;
+          top: -16rpx;
+          right: 160rpx;
+        }
+
+        .review-right {
+          position: absolute;
+          right: 0;
+        }
+      }
+
+      .review-list {
+        display: flex;
+        gap: 20rpx;
+        margin-bottom: 40rpx;
+
+        .review-img {
+          width: 80rpx;
+          height: 80rpx;
+          border-radius: 40rpx;
+        }
+
+        .content {
+          width: 580rpx;
+
+          .content-top {
+            position: relative;
+            height: 60rpx;
+            width: 580rpx;
+
+            .user-name {
+              position: absolute;
+              left: 0;
+            }
+
+            .review-time {
+              position: absolute;
+              right: 0;
+            }
+          }
+
+          .review-content {
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+          }
+        }
+      }
+    }
+  }
+
+  .richText {
+    margin: 20rpx;
+    width: 670rpx;
+  }
+
+  video {
+    display: block;
+    margin: 20rpx auto;
+    width: 710rpx;
+  }
+</style>
---
+++ otherPages/newsList/newsList.vue
@@ -0,0 +1,260 @@
+<template>
+  <view>
+    <view class="list-view">
+      <view class="list-item" v-for="item in list" :key="item.uid" @click="goDetails(item)">
+        <view class="left" v-if="item.node_image">
+          <image :src="getImagePath(item.node_image)"></image>
+        </view>
+        <view class="right">
+          <view class="title">
+            <view class="titleName">{{item.node_name || item.expert_name || ''}}</view>
+          </view>
+          <view class="from-and_date">
+            <text v-if="item.origin">{{item.origin}}|</text>
+            <text v-if="item.expert_title">{{item.expert_title}}|</text>
+            <text v-else>{{item.modify_time}}</text>
+            <text v-if="item.expert_org">{{item.expert_org}}</text>
+          </view>
+        </view>
+      </view>
+    </view>
+    <view class="isOver" v-if="isOver">{{ overText }}</view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tabSelected: "最新",
+        queryForm: {
+          columnRecommendation: '是'
+        },
+        pageIndex: 1,
+        pageSize: 10,
+        total: 0,
+        isOver: false,
+        overText: '---我是有底线的---',
+        list: [],
+        classify: "",
+        classifyObj: {
+          '头条资讯': 'fd41bbf8a61345bc8a22b499c9dcd046',
+          '直播': '0b0a342d55a74c70ac53854a028b7803',
+          '电视': '959930f21ddf443d9694a1b79168da4a',
+          '广播': 'fa6b29bca3fa47a2919df7bc1b30b992',
+        },
+        codeObj: {
+          '头条资讯': '1-TouTiaoZiXun',
+          '健康陕西': '2-JKSX',
+          '百姓健康栏目': '3-bxjkxllm',
+          '健康知识': '4-jkzs',
+          '科普资料': '5',
+          '宣传活动': '6',
+          '直播': '6-5',
+          '电视': '3-3-dslm',
+          '广播': '3-6-gb',
+        },
+        localdata: [],
+        classes: undefined,
+        map: {
+          text: 'classifyName',
+          value: 'uid'
+        },
+      }
+    },
+    onLoad(e) {
+      this.classify = e.classifyName
+      this.getList()
+    },
+    onReady() {
+      uni.setNavigationBarTitle({
+        title: this.classify + '列表'
+      })
+    },
+    methods: {
+      getList() {
+        const serviceMap = {
+          '中医健康科普专家': 'srvhealth_chinese_medicine_expert_select',
+          '中医历史与文化': 'srvhealth_knowledge_node_select'
+        }
+        const service = serviceMap[this.classify]
+        const url = this.getServiceUrl('health', service, 'select');
+        const req = {
+          "serviceName": service,
+          "colNames": ["*"],
+          "condition": [],
+          "page": {
+            "pageNo": this.pageIndex,
+            "rownumber": this.pageSize
+          }
+        }
+        if (this.classify === '') {
+          req.condition.push({
+            "colName": "parent_no",
+            "value": "KN2406210001",
+            "ruleType": "eq"
+          })
+        }
+        this.$http.post(url, req).then(res => {
+          if (res.data.state === 'SUCCESS') {
+            this.list = [...this.list, ...res.data.data]
+            this.total = res.data.page.total
+            if (this.pageIndex * this.pageSize >= this.total) {
+              this.isOver = true;
+            }
+          }
+        })
+      },
+      goDetails(data) {
+        uni.navigateTo({
+          url: `/otherPages/details/details?id=${ data.id}&classifyName=${this.classify}`
+        })
+      },
+      removeEmptyChildren(node) {
+        node.map(item => {
+          if (!item.children || item.children.length === 0) {
+            delete item.children
+          } else {
+            let res = item.children
+            this.removeEmptyChildren(res)
+          }
+        })
+        return node
+      },
+    },
+    // 下拉
+    onReachBottom() {
+      if (this.pageIndex * this.pageSize >= this.total) {
+        this.isOver = true;
+        return;
+      } else {
+        this.pageIndex++
+        this.getList();
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  page {
+    background: white;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .top-tab-view {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 30rpx;
+
+    .left-tab {
+      display: flex;
+      align-items: center;
+      flex-wrap: wrap;
+
+      .tab-item {
+        width: 140rpx;
+        height: 48rpx;
+        margin-left: 30rpx;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 24rpx;
+        border-radius: 10rpx;
+        background: #FAFAFA;
+        color: #CCCCCC;
+      }
+
+      .tab-selected {
+        background: #DAFCF6;
+        color: #27A280;
+      }
+    }
+
+    .right-btn {
+      display: flex;
+      align-items: center;
+      font-size: 24rpx;
+      color: #CCCCCC;
+
+      image {
+        width: 32rpx;
+        height: 32rpx;
+        margin-right: 30rpx;
+        margin-left: 10rpx;
+      }
+    }
+  }
+
+
+  .list-view {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 0 30rpx;
+    box-sizing: border-box;
+
+    .list-item {
+      width: 100%;
+      display: flex;
+      padding: 30rpx 0;
+      border-bottom: solid 1rpx #F5F7F7;
+
+      .left {
+        image {
+          width: 180rpx;
+          height: 135rpx;
+          border-radius: 16rpx;
+        }
+      }
+
+      .right {
+        flex: 1;
+        margin-left: 30rpx;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        .title {
+          width: 480rpx;
+          color: #666666;
+          font-size: 30rpx;
+          font-weight: 500;
+          display: flex;
+          align-items: center;
+
+          .play-icon {
+            width: 26rpx;
+            height: 20rpx;
+            margin-right: 20rpx;
+          }
+
+          .titleName {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+          }
+        }
+
+        .from-and_date {
+          color: #999999;
+          font-size: 24rpx;
+        }
+      }
+    }
+  }
+
+  ::v-deep .input-value-border {
+    border: none;
+  }
+
+  .isOver {
+    text-align: center;
+    font-size: 12px;
+    color: #bbb;
+    height: 60rpx;
+    line-height: 60rpx;
+  }
+</style>
